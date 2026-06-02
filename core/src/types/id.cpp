151610df3e336fd7a4bfb798ec4da9892ecaa59c
Ra1w <ArtyomCh2007@yandex.ru>
2026-06-02 17:01:59 +0300

    Update id

--- core/src/types/id.cpp
+++ core/src/types/id.cpp
@@ -1,5 +1,6 @@
 #include "core/types/id.hpp"
 
+#include <atomic>
 #include <chrono>
 #include <iomanip>
 #include <random>
@@ -17,11 +18,12 @@ bool ID::operator<(const ID& other) const { return value_ < other.value_; }
 
 void ID::Validate(const std::string& value) {
   if (value.empty()) {
-    throw ValidationError("ID", "Value cannot be empty");
+    throw ValidationError("ID", "Identifier cannot be empty");
   }
   const std::string forbidden = " /\\?%*:|\"<> \t\n\r";
   if (value.find_first_of(forbidden) != std::string::npos) {
-    throw ValidationError("ID", "Contains forbidden characters");
+    throw ValidationError("ID",
+                          "Identifier contains forbidden characters or spaces");
   }
 }
 
@@ -29,22 +31,20 @@ ID ID::Generate() {
   static std::random_device rd;
   static std::mt19937 gen(rd());
   static std::uniform_int_distribution<> dis(0, 15);
+  static std::atomic<uint64_t> counter{0};
+
   std::stringstream ss;
   ss << std::hex << std::setfill('0');
+
   for (int i = 0; i < 12; ++i) {
     ss << dis(gen);
   }
+
   auto now = std::chrono::system_clock::now().time_since_epoch().count();
   ss << "-" << std::hex << (now % 0xFFFF);
+  ss << "-" << std::hex << (counter.fetch_add(1) % 0xFFFF);
+
   return ID(ss.str());
 }
 
 }  // namespace core
-
-namespace std {
-
-size_t hash<core::ID>::operator()(const core::ID& id) const {
-  return std::hash<std::string>{}(id.Str());
-}
-
-}  // namespace std
