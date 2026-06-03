be6f50675d40ad2937b5c33addb7580a2e282ec6
Ra1w <ArtyomCh2007@yandex.ru>
2026-06-03 06:24:04 +0300

    Fix tag

--- core/src/types/tag.cpp
+++ core/src/types/tag.cpp
@@ -6,6 +6,17 @@
 namespace core {
 
 Tag::Tag(std::string value) : StringWrapper(std::move(value)) {
+  size_t first = value_.find_first_not_of(" \t\r\n");
+  if (first != std::string::npos) {
+    value_.erase(0, first);
+    size_t last = value_.find_last_not_of(" \t\r\n");
+    if (last != std::string::npos) {
+      value_.erase(last + 1);
+    }
+  } else {
+    value_.clear();
+  }
+
   if (value_.empty()) {
     throw ValidationError("Tag", "Value cannot be empty");
   }
